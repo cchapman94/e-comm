4d030ac9c93fc8704ebdd2f2c2f843c313aeae72
Chantell Chapman <chantellochapman@gmail.com>
2021-01-23 20:58:40 -0800

    remove from cart

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -25,7 +25,10 @@
 
         <!--Display remove from cart -->
         <div class="col-sm-3">
-          <button class="btn btn-warning">Remove from Cart</button>
+
+          
+          <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning">Remove from Cart</a>
+        
         </div>
 
 
