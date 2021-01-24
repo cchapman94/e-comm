81b140d0f1f61fa052e84bc5e318aeb9d2420df3
Chantell Chapman <chantellochapman@gmail.com>
2021-01-23 20:06:41 -0800

    added cartlist page

---
+++ resources/views/cartlist.blade.php
@@ -0,0 +1,39 @@
+@extends('master')
+@section("content")
+<div class="custom-product">
+  <div class="col-sm-10">
+  <!--cartlist items -->
+    <div class="trending-wrapper">
+    	<h4>Result for Products</h4>
+    	@foreach($products as $item)
+    	<div class="row searched-item cart-list-divider">
+
+        <!-- Display only product images -->
+    	  <div class="col-sm-3">
+          <a href="detail/{{$item->id}}">
+            <img class="trending-img" src="{{$item->gallery}}">
+          </a>
+        </div>
+
+        <!-- Display only product name & description -->
+        <div class="col-sm-4">
+            <div class="">
+              <h2>{{$item->name}}</h2>
+              <h5>{{$item->description}}</h5>
+            </div>
+        </div>
+
+        <!--Display remove from cart -->
+        <div class="col-sm-3">
+          <button class="btn btn-warning">Remove from Cart</button>
+        </div>
+
+
+      </div>	
+      @endforeach
+    </div>
+
+  </div>
+
+</div>
+@endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -55,6 +55,12 @@
 		width: 500px !important;
 	}
 
+	.cart-list-divider {
+		border-bottom: 1px solid #ccc;
+		margin-bottom: 20px;
+		padding-bottom: 20px;
+	}
+
 </style>
 
 <!--
