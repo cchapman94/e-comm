f79b6a860ee5b0860e5011486c91e194037b4fdc
Chantell Chapman <chantellochapman@gmail.com>
2021-01-26 23:06:46 -0800

    moved style content to css file

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>E-comm Project</title>
+
+	<link rel="stylesheet" href="{{ asset('/css/style.css') }}"> 
+
 	<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -24,6 +27,8 @@
 	{{View::make('footer')}}
 	
 </body>
+
+<!--
 <style>
 	.custom-login {
 		height: 500px;
@@ -62,6 +67,7 @@
 	}
 
 </style>
+-->
 
 <!--
 <script>
