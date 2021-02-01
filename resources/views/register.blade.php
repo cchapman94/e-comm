28ccd5810dbd61207b200b16646e84552785193b
Chantell Chapman <chantellochapman@gmail.com>
2021-02-01 14:57:09 -0800

    updated registration page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,17 +4,20 @@
 	<div class="row">
 		<div class="col-sm-4 col-sm-offset-4">
 			<form action="register" method="POST">
+
+				<p class="register">Register</p>
+
 				@csrf
 				<div class="form-group">			  	
-					<label for="exampleInputEmail1">User Name</label>
+					<!-- <label for="exampleInputEmail1">User Name</label> -->
 					<input type="text" name="name" class="form-control" id="exampleInputEmail1" placeholder="User Name">
 				</div>
 				<div class="form-group">			  	
-					<label for="exampleInputEmail1">Email address</label>
+					<!-- <label for="exampleInputEmail1">Email address</label> -->
 					<input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
 				</div>
 				<div class="form-group">
-					<label for="exampleInputPassword1">Password</label>
+					<!-- <label for="exampleInputPassword1">Password</label> -->
 					<input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 				</div>
 			  	<button type="submit" class="btn btn-default">Register</button>
