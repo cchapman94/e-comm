b97f0aa3959bb94f3cbdba7f3068396b0763f047
Chantell Chapman <chantellochapman@gmail.com>
2021-02-02 19:13:59 -0800

    updated detail page

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -14,11 +14,12 @@
   <div class="row">
     <div class="col-sm-6">
       <img class="detail-img" src="{{$product['gallery']}}" alt="product detail page">
+
     </div>
     <div class="col-sm-6">
       <a href="/">Go Back</a>
       <h2>{{$product['name']}}</h2>
-
+      <h4>Category: {{$product['category']}}</h4>
       <div class="rating">
         <span class="fas fa-star fa-sm text-primary"></span>
         <span class="fas fa-star fa-sm text-primary"></span>
@@ -29,17 +30,29 @@
 
       <h3>Price: {{$product['price']}}</h3>
       <h4>Details: {{$product['description']}}</h4>
-      <h4>Category: {{$product['category']}}</h4>
+      
       <br><br>
       <form action="/add_to_cart" method="POST">
         @csrf
         <input type="hidden" name="product_id" value={{$product['id']}}>
-      <button class="btn btn-primary">Add to Cart</button>  
+      <button class="btn btn-light"><i class="fas fa-shopping-cart pr-2"></i> Add to Cart</button>
+
+      <!-- <button type="button" class="btn btn-light btn-md mr-1 mb-2"><i
+                class="fas fa-shopping-cart pr-2"></i> Add to Cart</button> -->
+       
       </form>
 
-       <br><br>
-      <button class="btn btn-success">Buy Now</button>
-      <br><br>
+      <button class="btn btn-primary">Buy Now</button>            
+
+      <!-- <button type="button" class="btn btn-primary btn-md mr-1 mb-2">Buy Now</button> -->
+
+      
+
+
+ 
+
+
+
 
     </div>
 
