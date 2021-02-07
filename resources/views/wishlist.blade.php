a6590ea434034f88549ab270bed7941b99a4f828
Chantell Chapman <chantellochapman@gmail.com>
2021-02-06 22:25:48 -0800

    added wishlist page

---
+++ resources/views/wishlist.blade.php
@@ -0,0 +1,58 @@
+@extends('master')
+@section("content")
+
+
+<div class="jumbotron color-grey-light mt-70">
+      <div class="d-flex align-items-center h-100">
+        <div class="container text-center py-5">
+          <h3 class="mb-0">Your Wishlist</h3>
+        </div>
+      </div>
+    </div>
+
+<div class="custom-product">
+  <div class="col-sm-10">
+  <!--cartlist items -->
+    <div class="trending-wrapper">
+    	@foreach($products as $item)
+    	<div class="row searched-item cart-list-divider">
+
+        <!-- Display only product images -->
+    	  <div class="col-sm-3">
+          <a href="detail/{{$item->id}}">
+            <img class="trending-img" src="{{$item->gallery}}">
+          </a>
+        </div>
+
+        <!-- Display only product name & description -->
+        <div class="col-sm-4">
+            <div class="">
+              <h2 class="cartName">{{$item->name}}</h2>
+              <h5 class="description">{{$item->description}}</h5>
+              <h5 class="price">${{$item->price}}</h5>
+             
+            </div>
+        </div>
+
+        <!--Display remove from cart -->
+        <div class="col-sm-3">
+          <div class="rmvCart">
+
+          
+          <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning"><i class="fas fa-trash-alt"></i> Remove from Cart</a>
+        </div>
+        
+        </div>
+
+        
+
+      </div>	
+
+      @endforeach
+    
+    </div>
+
+  </div>
+
+</div>
+@endsection
